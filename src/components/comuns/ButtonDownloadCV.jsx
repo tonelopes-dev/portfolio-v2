f8a2e0ee66f1d0033ad0115992659def2ac53c03
Tone Lopes <tonelopes.dev@gmail.com>
2023-11-27 13:42:37 -0300

    feat: adicionando os links dos botões.

--- src/components/comuns/ButtonDownloadCV.jsx
+++ src/components/comuns/ButtonDownloadCV.jsx
@@ -2,15 +2,19 @@ import React from "react";
 
 const ButtonDownloadCV = () => {
   return (
-    <button
-      type="button"
-      className="mb-2 me-2 inline-flex   items-center  rounded-lg  bg-blue-700 px-5  py-2.5   text-center text-sm  font-bold  text-white  hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600  dark:hover:bg-blue-700 dark:focus:ring-blue-800 "
+    <a
+      href="https://drive.google.com/file/d/1N6GA_U2Sf1BtYO7Dr5Sz8dJ3QDy3JHVW/view?usp=sharing"
+      target="_blank"
+      className="ml-3"
     >
-      <i className="fa-solid fa-download "></i>
-      <a href="" target="_blank" className="ml-3">
+      <button
+        type="button"
+        className="mb-2 me-2 inline-flex   items-center  rounded-lg  bg-blue-700 px-5  py-2.5   text-center text-sm  font-bold  text-white  hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600  dark:hover:bg-blue-700 dark:focus:ring-blue-800 "
+      >
+        <i className="fa-solid fa-download "></i>
         Download CV
-      </a>
-    </button>
+      </button>{" "}
+    </a>
   );
 };
 
