9d720e0df9191d9e189e83653ea893c8a5a3c45d
Tone Lopes <tonelopes.dev@gmail.com>
2023-11-28 15:09:22 -0300

    feat: adicionando um foto para o banner.

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -6,7 +6,7 @@ import ButtonDownloadCV from "/src/components/comuns/ButtonDownloadCV";
 const Banner = () => {
   return (
     <div className="lg:h-600 bg-banner flex h-2/4 flex-col-reverse justify-center  bg-gray-50 bg-opacity-90 px-8 pb-4 hover:bg-white md:flex-row">
-      <div className=" flex  flex-col  justify-center">
+      <div className=" flex flex-1 flex-col  justify-center">
         <h1 className="lg:text-1xl mb-4 text-left text-3xl font-extrabold text-gray-900 dark:text-white md:text-4xl">
           <span className="bg-gradient-to-r from-blue-800 to-indigo-500 bg-clip-text text-transparent ">
             Desenvolvedor Backend
@@ -24,12 +24,11 @@ const Banner = () => {
             <ButtonDownloadCV />
           </div>
         </div>
-       
       </div>
       <div className="h-max-img w-auto  items-center justify-center ">
         <img
           className="mx-auto h-full w-auto py-4"
-          src="https://raw.githubusercontent.com/tonelopes-dev/portfolio-v2/main/src/assets/tone-sobre.png"
+          src="https://raw.githubusercontent.com/tonelopes-dev/portfolio-v2/main/src/assets/tone-sobre-sm.png"
           alt="Descrição da imagem"
         />
       </div>
