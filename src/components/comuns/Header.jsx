e00a0934e68c098d651100d87693156ea5fb15da
Tone Lopes <tonelopes.dev@gmail.com>
2023-11-28 21:21:48 -0300

    fix: correção abrindo menu mobile.

--- src/components/comuns/Header.jsx
+++ src/components/comuns/Header.jsx
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "./Logo";
 import { Link } from "react-router-dom";
+import * as NavigationMenu from "@radix-ui/react-navigation-menu";
+import classNames from "classnames";
+import "./styles.css";
 
 const Header = () => {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+  const [isProjectOpen, setProjectOpen] = useState(false);
+
+  const ListItem = React.forwardRef(
+    ({ className, children, title, ...props }, forwardedRef) => (
+      <li>
+        <NavigationMenu.Link asChild>
+          <a
+            className={classNames("ListItemLink", className)}
+            {...props}
+            ref={forwardedRef}
+          >
+            <div className="ListItemHeading">{title}</div>
+            <p className="ListItemText">{children}</p>
+          </a>
+        </NavigationMenu.Link>
+      </li>
+    ),
+  );
+
   return (
     <div>
-      <nav className="rounded-t bg-blue-400 shadow ">
+      <nav className="rounded-t bg-blue-400 shadow">
         <div className="mx-auto flex max-w-screen-xl flex-wrap items-center justify-between p-4">
           <Link to="/" className="flex items-center">
             <Logo />
@@ -14,11 +37,11 @@ const Header = () => {
             </span>
           </Link>
           <button
-            data-collapse-toggle="navbar-default"
             type="button"
-            className="inline-flex h-10 w-10 items-center justify-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200  md:hidden"
+            className="inline-flex h-10 w-10 items-center justify-center rounded-lg p-2 text-sm text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 md:hidden"
             aria-controls="navbar-default"
-            aria-expanded="true"
+            aria-expanded={isMenuOpen}
+            onClick={() => setMenuOpen(!isMenuOpen)}
           >
             <span className="sr-only">Open main menu</span>
             <svg
@@ -38,10 +61,12 @@ const Header = () => {
             </svg>
           </button>
           <div
-            className="hidden w-full bg-transparent md:block md:w-auto"
+            className={`${
+              isMenuOpen ? "" : "hidden "
+            }w-full bg-transparent md:block md:w-auto`}
             id="navbar-default"
           >
-            <ul className="mt-4 flex flex-col items-center rounded-lg border border-gray-100 font-medium dark:border-gray-700 dark:bg-gray-800 md:mt-0 md:flex-row md:space-x-8 md:border-0">
+            <ul className="my-4 flex flex-col items-center rounded-lg border border-gray-100 py-4 font-medium dark:border-gray-700 dark:bg-gray-800 md:mt-0 md:flex-row md:space-x-8 md:border-0">
               <li>
                 <Link
                   to="/"
@@ -62,10 +87,9 @@ const Header = () => {
 
               <li>
                 <button
-                  id="dropdownDividerButton"
-                  data-dropdown-toggle="dropdownDivider"
-                  className=" inline-flex items-center rounded-lg px-5 py-6 pl-3 pr-4 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300  md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 "
+                  className="text-md inline-flex items-center rounded-lg px-5 py-3 pl-3 pr-4 text-center font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300  md:border-0 md:p-0 md:hover:bg-transparent md:hover:text-blue-700 "
                   type="button"
+                  onClick={() => setProjectOpen(!isProjectOpen)}
                 >
                   Projetos
                   <svg
@@ -85,21 +109,14 @@ const Header = () => {
                   </svg>
                 </button>
                 <div
-                  id="dropdownDivider"
-                  className="z-10 hidden w-44 divide-y divide-gray-100 rounded-lg bg-white shadow dark:divide-gray-600 dark:bg-gray-700"
+                  className={`${
+                    isProjectOpen ? "" : "hidden "
+                  }z-10 mt-3 w-44 divide-y divide-gray-100 rounded-lg bg-white shadow dark:divide-gray-600 dark:bg-gray-700`}
                 >
-                  <ul
-                    className="py-2 text-sm text-gray-700 dark:text-gray-200"
-                    aria-labelledby="dropdownDividerButton"
-                  >
+                  <ul className="py-2 text-sm text-gray-700 dark:text-gray-200">
                     <li>
                       <a
                         href="#projects"
-                        data-tabs-target="#profile"
-                        type="button"
-                        role="tab"
-                        aria-controls="profile"
-                        aria-selected="false"
                         className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
                       >
                         🖼️ Frontend
