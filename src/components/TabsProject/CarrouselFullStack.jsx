183598f2918ba76c8ca2fdbc098fd8420c6ab0f9
Tone Lopes <tonelopes.dev@gmail.com>
2025-07-17 15:14:32 -0300

    chore: remover importações desnecessárias de pagination e navigation nos componentes de carrossel

--- src/components/TabsProject/CarrouselFullStack.jsx
+++ src/components/TabsProject/CarrouselFullStack.jsx
@@ -3,8 +3,6 @@ import { Swiper, SwiperSlide } from 'swiper/react'
 
 // Import Swiper styles
 import 'swiper/css'
-import 'swiper/css/pagination'
-import 'swiper/css/navigation'
 import 'swiper/swiper-bundle.css'
 
 // import required modules
